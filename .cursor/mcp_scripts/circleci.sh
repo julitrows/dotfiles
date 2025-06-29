c53b76765d9d42efc996b63ded2ad26ea162bb12
Julio AG <julio@rubiconmd.com>
2025-06-29 14:17:13 +0200

    chore: add cursor mcp config

---
+++ .cursor/mcp_scripts/circleci.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+
+source ~/.zshrc && npx -y @circleci/mcp-server-circleci
---
+++ .cursor/mcp_scripts/github.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+
+source ~/.zshrc && GITHUB_PERSONAL_ACCESS_TOKEN=$GITHUB_MCP_PAT /Users/julio/Code/github-mcp-server/cmd/github-mcp-server/github-mcp-server stdio
